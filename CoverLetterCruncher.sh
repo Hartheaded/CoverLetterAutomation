c0610b97cfcb5d64319ad026f0fd3229ab720356
Hartheaded <158302550+Hartheaded@users.noreply.github.com>
2024-11-20 14:57:51 -0600

    Update CoverLetterCruncher.sh
    Took a break from this, and when I came back it was pretty bad IMO. I changed a lot, but the main thing I wanted to note here is that I...
    ~ Rearranged objects to a better order
    ~ Replaced all instances of functions using other functions making things easier to read (for me at least)
    ~ Changed templateSelection to better filter out bad user inputs. Only accepts numbers ranging from 1 to $count
    + added check to see if a resume exists before executing mergeToResume
    + added comments to everything explaining exactly how this script works
    - Removed some old comments

--- CoverLetterCruncher.sh
+++ CoverLetterCruncher.sh
@@ -2,48 +2,36 @@
 
 # This script is aimed to take one of the files within the templates folder, change the company title and job title of the file in .tmp, then export it as a PDF in the output folder. Each of these PDFs should be named in a style similar to this [Company][Job Title][Date].pdf
 
-# Takes the contents of ./appendText.txt and appends it to the bottom of the output of inputVerifier
-appendText(){
-	if [[ $(wc appendText.txt) != "0 0 0 appendText.txt" ]]; then
-		cat "./appendText.txt" >> $template
-		echo "Appended $templateOutput with ./appednedText.txt"
-	else
-		echo "appendText.txt file is empty"
-		echo "Skipping. . ."
-	fi
-}
 
-# Converts the temporary file found in ./.tmp/ to a pdf file in ./output/
-pdfMaker(){
-	libreoffice --convert-to "pdf" --outdir output $template
-}
-i
 # DANGEROUS WILL DELETE EVERYTHING INSIDE OF /.tmp/
 cleanUp(){
 	rm -Rf .tmp/*
 }
 
-
-# Select a template; Verify Selection
+# Generates a numbered list of templates to choose from, confirms data, and sets user's $templateChoice to the full directory of the template
 templateSelection(){
 	while true; do
-		echo "Please select a template"
-		cleanUp
 		count=0
 		for i in ./templates/* ; do
 			count=$((count + 1))
 			echo "$count. $i"
 			echo "$count. $i" | cat >> .tmp/count.txt
-		done	
-		read templateChoice
-	
-		echo "$(cat .tmp/count.txt | grep "$templateChoice" )" &> .tmp/count.txt
-		templateChoice=$(cat .tmp/count.txt)
-		templateChoice=${templateChoice:2}
-		break
+		done
+		
+			read -p "Please select a template (1-$count)" templateChoice
+				case $templateChoice in
+					[0-$count]) 
+				echo "$(cat .tmp/count.txt | grep "$templateChoice" )" &> .tmp/count.txt
+				templateChoice=$(cat .tmp/count.txt)
+				templateChoice=${templateChoice:2}
+				return 0
+						;;
+					* ) echo "Please select using a number..." ;;
+				esac	
 	done
 }
 
+# Asks user to input the company name and job title for the $companyName and $jobTitle variables
 positionInfo(){
 	while true; do
 		echo "What is the name of the company?"
@@ -62,14 +50,58 @@ positionInfo(){
 	done
 }
 
-cleanUp
-templateSelection
-positionInfo
-template=".tmp/${companyName//[[:blank:]]/}-${jobTitle//[[:blank:]]/}-$(date +%d-%m-%y).txt"
-cp -f $templateChoice $template
-sed -i "s/#COMPANYNAME#/$companyName/g" "$template"
-sed -i "s/#JOBTITLE#/$jobTitle/g" "$template"
-appendText
-templateOutput="output/$companyName-$jobTitle-$(date +%d-%m-%y).txt"
-pdfMaker
+# Appends the document with text from ./appendText.txt
+appendText(){
+	if [[ $(wc appendText.txt) != "0 0 0 appendText.txt" ]]; then
+		cat "./appendText.txt" >> $template
+		echo "Appended cover letter with ./appednedText.txt"
+	else
+		echo "appendText.txt file is empty"
+		echo "Skipping. . ."
+	fi
+}
 
+# Converts the temporary file found in ./.tmp/ to a pdf file in ./output/
+pdfMaker(){
+	libreoffice --convert-to "pdf" --outdir output $template
+}
+
+# Optional step when you can only give a resume. Merges the resume and cover letter together.
+mergeToResume(){
+	while true; do
+		read -p "Do you wish to append this document to your resume? (Y/N)" response
+		case $response in
+			# swap the first two parameters in the pdfunite to merge the resume UNDER the generated cover letter
+			[Yy]* ) pdfunite $($resume) $($pdfOut) $($pdfOut) && break;;
+			[Nn]* ) break;;
+			* ) echo "Your choices are (Y/N)"
+		esac
+	done
+}
+
+# Clean up .tmp
+cleanUp
+# Select a template from ./templates/
+templateSelection
+# User inputs company name and job title
+positionInfo
+# Sets the output filename ./output/CompanyTitle-JobTitle-01-02-03.pdf
+pdfOut="./output/${companyName//[[:blank:]]/}-${jobTitle//[[:blank:]]/}-$(date +%d-%m-%y).pdf"
+# Sets the working template file name as variable. Similar to above, but a .txt file
+template="./.tmp/${companyName//[[:blank:]]/}-${jobTitle//[[:blank:]]/}-$(date +%d-%m-%y).txt"
+# Used for mergeToResume. You will need to supply your OWN resume for this function to work
+resume="./resume.pdf"
+# Copies the selected template and puts it in .tmp for further processing
+cp -f $templateChoice $template
+# Scans through file and replaces all instances of #COMPANYNAME# to the peviously set $companyName
+sed -i "s/#COMPANYNAME#/$companyName/g" "$template"
+# Same thing as above, but with #JOBTITLE# and the job title.
+sed -i "s/#JOBTITLE#/$jobTitle/g" "$template"
+# Appends the text from ./appendText.txt to the bottom of the text file being worked on in .tmp
+appendText
+# Turns the entire document into a pdf file and places the results in the ./output/ folder
+pdfMaker
+# Optional step; Asks user if they want to merge their cover letter to their $resume
+if [[ -e $resume ]]; then
+	mergeToResume
+fi
